24bd6c0c30994f3fef417bbb958b3b1d58a95992
aandrukhovich <aaandrukhovich@yandex.ru>
2021-02-22 03:01:49 +0300

    Add csv2futcheap to read csv and return list of Players

--- futcheap/data/csv2futcheap.py
+++ futcheap/data/csv2futcheap.py
@@ -1,2 +1,30 @@
+import csv
+from futcheap.lib.player import Player
+
+
+# fill list of clubs, nations, leagues and card types
 def load_csv(csv_filepath: str):
     """ return list of Player"""
+    clubs = set()
+    nations = set()
+    leagues = set()
+    card_types = set()
+    players = []
+    with open(csv_filepath, 'r', newline='') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            players.append(Player(row))
+            clubs.add(row['club'])
+            nations.add(row['nation'])
+            leagues.add(row['league'])
+            for card_type in row['card_types'].split(' '):
+                card_types.add(card_type)
+    print(len(clubs))
+    print(len(nations))
+    print(len(leagues))
+    print(sorted(list(card_types)))
+    return players, clubs, nations, leagues, card_types
+
+
+if __name__ == '__main__':
+    players = load_csv('csvs/data_660.csv')
