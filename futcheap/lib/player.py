a688d66e942ffa21b6117a2ad22b7286cb36a7e1
aandrukhovich <aaandrukhovich@yandex.ru>
2021-02-22 04:05:55 +0300

    Transfer price converter to player.py (not the best solution)

--- futcheap/lib/player.py
+++ futcheap/lib/player.py
@@ -1,6 +1,14 @@
 from typing import Dict
-from futcheap.utils import utils
 
+def convert_price_to_int(price: str) -> int:
+    multiplier = 1
+    if price.endswith('M'):
+        multiplier = 1_000_000
+        price = price[:-1]
+    elif price.endswith('K'):
+        multiplier = 1_000
+        price = price[:-1]
+    return int(float(price) * multiplier)
 
 class Player:
     def __init__(self, content: Dict):
@@ -8,14 +16,11 @@ class Player:
         self.club = content['club']
         self.nation = content['nation']
         self.league = content['league']
-        self.rating = content['rating']
+        self.rating = int(content['rating'])
         self.position = content['position']
-        self.price = utils.convert_price_to_int(content['price'])
+        self.price = convert_price_to_int(content['price'])
         self.card_types = content['card_types']
 
 
     def __str__(self):
         return f"name={self.name} rating={self.rating} price={self.price}"
-
-
-
--- futcheap/utils/utils.py
+++ futcheap/utils/utils.py
@@ -5,15 +5,6 @@ from futcheap.lib.schema import Schema
 from futcheap.lib.player import Player
 
 
-def convert_price_to_int(price: str) -> int:
-    multiplier = 1
-    if price.endswith('M'):
-        multiplier = 1_000_000
-        price = price[:-1]
-    elif price.endswith('K'):
-        multiplier = 1_000
-        price = price[:-1]
-    return int(float(price) * multiplier)
 
 POSITIONS_LIST = [
     "GK",  # 0
